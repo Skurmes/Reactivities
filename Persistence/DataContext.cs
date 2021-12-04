489a4a092cd738745d27ad3986b411d23a1c9eec
Tristan Wood <triawoo@email.ecok.edu>
2021-12-04 04:57:13 -0600

    Section 2

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,7 +6,7 @@ namespace Persistence
     public class DataContext : DbContext
     {
         
-        public DataContext( DbContextOptions options) : base(options)
+        public DataContext(DbContextOptions options) : base(options)
         {
 
         }
