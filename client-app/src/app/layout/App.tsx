ec2e4bb70a8798be5b7c6f788917286f76193db4
Tristan Wood <triawoo@email.ecok.edu>
2021-12-16 10:01:47 -0600

    Part of Section 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-const [activities, setActivities] = useState([]);
+const [activities, setActivities] = useState<Activity[]>([]);
 
 useEffect(() => {
-  axios.get('http://localhost:5000/api/activities').then(response => {
-  console.log(response);  
-  setActivities(response.data);
+  axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => { 
+    setActivities(response.data);
   })
 }, [] /*This array keeps the useEffect protery from continously running */);
 
   return (
     <div>
       <Header as='h2' icon='users' content='Reactivities'/>
+        
         <List>
-          {activities.map((activity: any) =>
+          {activities.map(activity =>
             (
             <List.Item key={activity.id}>
               {activity.title} {/* This will display the activity titles. */}
---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,9 @@
+export interface Activity {
+    id: string;
+    title: string;
+    date: Date; //This might need to change to string.
+    description: string;
+    category: string;
+    city: string;
+    venue: string;
+}
